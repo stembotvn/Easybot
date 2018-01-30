3936cf9e970f90cae0305f0976f826c33dd290d1
STEMbot <stembot.vn@gmail.com>
2018-01-31 01:54:02 +0700

    update for BLE control on IOS

--- examples/EasybotBLEcontrol/EasybotBLEcontrol.ino
+++ examples/EasybotBLEcontrol/EasybotBLEcontrol.ino
@@ -17,72 +17,82 @@ Chip điều khiển động cơ: L9110S
 Sensor: 3 IR Line detect sensor
         Cảm biến siêu âm đo khoảng cách SRF04
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-Chương trình mẫu điều khiển SmartCar qua Android SmartPhone, sử dụng App: Arduino Bluetooth RC Car của tác giả Andi.Co 
-Link: https://play.google.com/store/apps/details?id=braulio.calle.bluetoothRCcontroller&hl=en   
-Sử dụng: Gắn module HC05/HC06 vào cổng HC05 trên Board Easybot Nano 
-      Easybot Nano (Vcc Gnd Rx Tx) <--> HC05/06(Vcc Gnd Tx Rx)
-      Bật chức năng Bluetooth trên điện thoại Android, Scan BlueTooth Device tìm module HC05 -> Nhập Pass 1234 để pair 2 thiết bị 
-      Vào App Bluetooth RC Control, vào Setting, chọn "Connect to car" , chọn module HC05 (đã Paired) trong list thiết bị, nếu kết nối thành công thì đèn kết nối ở góc trái sáng màu xanh
-      Điều khiển Robot với các chức năng cơ bản: tới, lùi, quay trái, quay phải. 
+Chương trình mẫu điều khiển SmartCar trên SmartPhone qua Bluetooth Low Energy (Bluetooth 4.0) dựa trên chip CC2540, sử dụng App BLEJoystick cho cả IOS và Android
+Android app Link :https://play.google.com/store/apps/details?id=iyok.com.blejoystick&hl=en  
+IOS app Link: https://itunes.apple.com/us/app/ble-joystick/id1098200556?mt=8
+Sử dụng: Gắn module JYD-08 hoặc HM-10 vào khe cắm Bluetooth 
+      Bật bluetooth trên điện thoại
+      Mở App BLEJoystick, bấm vào biểu tượng Bluetooth -> Scan BlueTooth Device tìm module JDY-08 hoặc HM-10  để kết nối
+      
             
 *----------------------------------------------------------------------------------------------------------------*/
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include "Easybot.h"
-///
+
 EasybotNano Robot; 
 SoftwareSerial BT(BT_Rx_Pin,BT_Tx_Pin);
+char cmd;
 
-int val = 0;
-int horizontal = 50;
-int vertical = 50;
-int A_state = 0;
-int B_state = 0;
-char data[5];
-int m1,m2;
 void setup() {
 
-BT.begin(9600);         // Bluetooth baurate Speed is 9600 bps
+BT.begin(115200);         // Bluetooth baurate Speed is 115200 bps for JDY-08 Module, 9600 for HM-10 module
 Serial.begin(9600);     //Serial Monitor baurate Speed is 9600;
-Serial.println(" Example for BLE Control via IOS and Android LOFI App ");
+Serial.println(" Example for BLE Control on IOS and Android BLEJoystick App ");
 delay(2000);
+
 }
+
+
 
 void loop() 
 {
-if (BT.available()) 
+
+if (BT.available()>0) 
   {
-  BT.readBytes(data,5);
-//  Serial.println("------------------------------");
-  for (int i=0;i<5;i++)
-   {
-    Serial.print((int)data[i]); Serial.print(" ");
-    }
-    Serial.println(" ");
-/*
-    horizontal = int(data[1]); // joystick os pozioma
-    vertical = int(data[2]); // joystick os pionowa
-    A_state = int(data[3]); // lewy przycisk
-    B_state = int(data[4]); // prawy przycisk
-    
-    horizontal = horizontal * 2 - 100;
-    vertical = vertical * 2 - 100;
-    Serial.print("horizontal = ");
-    Serial.print(horizontal);
-    Serial.print(" vertical = ");
-    Serial.print(vertical);
-    Serial.print(" A = ");
-    Serial.print(A_state);
-    Serial.print(" B = ");
-    Serial.println(B_state);
-  //  horizontal = horizontal * 2 - 100;
-  //   vertical = vertical * 2 - 100;
-    m1 = vertical - horizontal; //Serial.print("ML = ");Serial.println(m1);
-    m2 = vertical + horizontal;// Serial.print("MR = ");Serial.println(m2);
-  int mm1 = min(255, 2.7 * abs(vertical - horizontal));
-  int mm2 = min(255, 2.7 * abs(vertical + horizontal));
-  Serial.print("M1= ");Serial.print(mm1);Serial.print("   M2 = ");Serial.println(mm2);
-  //  Robot.moveForward(m1,m2); */
+  cmd = BT.read();
+  Serial.print(cmd);
+  Serial.println((int)cmd);
   }
+  
+ 
+
+  switch (cmd) {
+   case 0: 
+     Robot.stop();
+      break;
+   case 'A':
+     Robot.moveForward(50);
+      break;
+   case 'a':
+     Robot.moveForward(100);
+      break;   
+   case 'B':
+     Robot.turnRight(50);
+     break;
+   case 'b':
+     Robot.turnRight(70);
+     break;  
+   case 'D':
+     Robot.turnLeft(50);
+     break;
+   case 'd':
+     Robot.turnLeft(70);
+     break;  
+   case 'C':
+     Robot.moveBack(60); 
+     break;
+    case 'c':
+     Robot.moveBack(100); 
+     break;  
+     case 'H':
+     Robot.stop(); 
+     break;   
+   default: 
+  
+     break;         
+ 
+     }
+  
+  // else Robot.stop();
 
 }
-
