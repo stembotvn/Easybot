579c32f5686a825e0879ba31682ca9354af911eb
stembotvn <stembot.vn@gmail.com>
2018-03-20 12:00:53 +0700

    comment Start

--- examples/Lessons/Lesson1_Moving/Lesson1_Moving.ino
+++ examples/Lessons/Lesson1_Moving/Lesson1_Moving.ino
@@ -6,7 +6,7 @@ EasybotNano Robot; //tạo đối tượng Robot
  
 void setup() {
 
-Robot.waitStart(5);
+Robot.waitStart(5); // Chờ phất dấu hiệu khởi động trước mặt robot để bắt đầu
 Serial.println("START!");  
 Robot.moveForward(100);
 }
