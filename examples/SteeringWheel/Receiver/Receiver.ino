87785e8075a36f9fd02442d4f6fa93e0613e284b
stembotvn <stembot.vn@gmail.com>
2018-04-20 17:50:45 +0700

    update

---
+++ examples/SteeringWheel/Receiver/Receiver.ino
@@ -0,0 +1,87 @@
+
+
+#include "Easybot.h"
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#define CE_PIN   7
+#define CSN_PIN  8
+
+#define offset      20   // stop if below value 
+#define Range       80   // Max speed 
+
+EasybotNano Robot; 
+int speed=100;
+byte address[1][6] = {"robot"};              // Radio pipe addresses for the 2 nodes to communicate.
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+/*-----Object Declaration ----*/
+
+RF24 radio(CE_PIN, CSN_PIN); // Activate the Radio
+
+/*-----Declaration of Variables -----*/
+
+int data[4];  //  Two element array holding the data readings
+bool debug = 1; 
+int l,r;
+int Cx,Cy,Cz;
+bool updated = 0;
+void setup()  
+{
+  Serial.begin(9600); /* Opening the Serial Communication */
+
+  Serial.println("Nrf24L01 Receiver Starting");
+  radio.begin();
+    radio.setChannel(108);
+    radio.setDataRate(RF24_1MBPS);    // Tốc độ truyền
+ radio.setPALevel(RF24_PA_HIGH);
+   //radio.setAutoAck(0);
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
+  l=0;r=0;  //left speed = 0; right speed = 0; 
+    delay(1000);
+}//--(end setup )---
+
+
+void loop()   
+{
+  if ( radio.available() )
+  {
+    // Reading the data payload until the RX received everything
+  //done = false;
+   while (radio.available()) {                                   // While there is data ready
+        radio.read( data, sizeof(data));    // Get the payload
+        updated = 1; 
+       
+      }
+   Serial.println("updated"); 
+   Serial.println(data[3]);   
+  }
+  
+  if(updated){
+     Cx = data[0];
+     Cy = data[1];
+     Cz = data[2];
+     Serial.print(Cx);Serial.print(",");Serial.println(Cy);
+
+    
+      l = Cy+Cx; r = Cy-Cx; 
+      if (l>Range) l = Range; 
+      else if (l<-Range) l = -Range;
+      if (r>Range) r = Range;
+      else if (r<-Range) r=-Range; 
+     
+      if((Cy<offset && Cy >-offset) && (Cx <offset && Cx >-offset))
+          {
+            Robot.stop();
+          }
+      else   Robot.moveForward(l,r);
+      updated = 0;
+      }
+      
+      
+      
+      
+   
+    
+ }
---
+++ examples/SteeringWheel/Transmitter/Transmitter.ino
@@ -0,0 +1,123 @@
+
+/*Easybot is a simple designed for early learing code for kids, this library . This is one of STEM education projects of Things Changing Technology Co. Ltd
+Link: http://stembot.vn
+ Easybot is suitable for 9-15 years old.
+ Easybot is compatible with SmartCar Robot Models
+
+*--Robot:
+Core: Arduino Nano (Protoype)/Arduino Mini (Product)
+Motor Driver: L9110S
+
+Sensor: IR reflect sensor x 3 for line following 
+        Sonar Range Finder sensor:  SRF04
+---------------------------------------------------------------------------------------------------------------------
+-Chương trình mẫu cho Smart Glove Control Robot - (Transmitter mode)
+            
+*----------------------------------------------------------------------------------------------------------------*/
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
+#define CE_PIN   7
+#define CSN_PIN 8
+#define x A2
+#define y A1
+#define z A0
+#define debug_EN 2
+#define Maxspeed    80
+
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+//const uint64_t pipe = 0xE8E8F0F0BBLL;
+//const uint64_t pipe = 0xE8E8F0F0CCLL;
+byte address[2][6] = {"robot1","robot2"};      
+RF24 radio(CE_PIN, CSN_PIN); // Activate  the Radio
+int button=0;
+int old_button=0;
+int data[4];  // Two element array holding the Joystick readings
+int old_data[4];
+bool updated = 0;
+int Cx,Cy,Cz,RCx,RCy,RCz;
+
+int l = 0;
+int r = 0;
+int Calib_Max =400;
+int Calib_Zero = 330 ;
+//int Calib_Max =400;
+//int Calib_Zero = 330 ;
+int Calib_Min = 2*Calib_Zero - Calib_Max;
+
+
+void readsensor()
+{
+  RCx = analogRead(x); //Serial.print("Raw_X = "); Serial.print(Cx);
+  RCy = analogRead(y);// Serial.print("__Raw_Y = "); Serial.print(Cy);
+  RCz = analogRead(z);// Serial.print("__Raw_Z = "); Serial.println(Cz);
+ if (!digitalRead(debug_EN))
+ {
+    Serial.print("Raw_X = "); Serial.print(RCx);
+    Serial.print("__Raw_Y = "); Serial.print(RCy);
+    Serial.print("__Raw_Z = "); Serial.println(RCz);
+    
+  }
+   Cx = map(RCx, Calib_Min, Calib_Max , -Maxspeed, +Maxspeed);
+   Cy = map(RCz, Calib_Min, Calib_Max , +Maxspeed, -Maxspeed);
+     // Cz = map(RCz, Calib_Min, Calib_Max , +Maxspeed, -Maxspeed);
+  if (!digitalRead(debug_EN)) 
+  
+{
+    Serial.print(Cx);Serial.print(",");Serial.println(Cy);
+    
+  }
+ //delay(1000);
+  //Plotting Data to Graphic 
+  //Serial.print(RCx);Serial.print(",");Serial.println(RCy);Serial.print(",");
+  
+
+/*  
+  Serial.print("Cx = ");
+  Serial.print(Cx);
+  Serial.print(" --- ");
+  Serial.print("Cy = ");
+  Serial.print(Cy);
+  Serial.print(" --- ");
+  Serial.print("Cz = ");
+  Serial.println(Cz);
+  */
+ 
+ 
+}
+void setup()   
+{
+   Serial.begin(9600); 
+
+  pinMode(debug_EN,INPUT_PULLUP);
+  radio.begin();
+  radio.setChannel(108);
+  radio.setDataRate(RF24_1MBPS);    // Tốc độ truyền
+  radio.setPALevel(RF24_PA_HIGH);
+   radio.openWritingPipe(pipe);
+  Serial.println("Start");
+  delay(1000);
+}
+
+
+void loop()   
+{
+  readsensor();
+  data[0] = Cx;
+  data[1] = Cy;
+  data[2] = Cz;
+  data[3] = 127; //for test transmitter
+//  if (data[0]!=old_data[0] || data[1] != old_data[1] || data[2] != old_data[2]) 
+//     { updated = 1; old_data[0] = data[0];old_data[1]=data[1];old_data[2]=data[2];}
+
+//if (updated) 
+ {
+   if (radio.write( data, sizeof(data) )) Serial.println("Data sent!");
+   else Serial.println("Fail!");
+ /*  radio.write(data,sizeof(data)); 
+   bool ok = radio.txStandBy();
+   if (ok) Serial.println("Data sent!");
+   else Serial.println("Fail!");*/
+  //  ;updated = 0;    
+ }
+}
